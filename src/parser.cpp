28bf391505c1192e66a44a84500bd74c7bc18668
Jon Leithe <jon@jonleithe.no>
2024-10-29 21:00:34 +0100

    Re-structured the input parsing. Now using a state machine - I'm pretty darn pleased with it :-)

--- src/parser.cpp
+++ src/parser.cpp
@@ -32,7 +32,8 @@ enum class State{	READY,
 					DEL,
 					HORIZONTAL_DIVIDER,
 					FOOTER,
-					CHECK_NEW_LINE
+					CHECK_NEW_LINE,
+					ADD_LIST_ITEM
 };
 
 
@@ -75,7 +76,6 @@ void ivsSessions::parser(void)
 									break;
 		case State::NEW_LINE:		if(*ptr == '\r'){
 										buffer.append(1, '\0');
-										state = State::READY;
 										*ptr++;
 										fo_inputBufferIndex++;
 										state = State::CHECK_NEW_LINE;
@@ -113,18 +113,202 @@ void ivsSessions::parser(void)
 									break;
 		case State::SESSION_TYPE:	if(*ptr == '|'){
 										_sessionType.append(1, '\0');
-										state = State::SESSION_CODE;
-										*ptr++;
-										fo_inputBufferIndex++;
+										state = State::DATE;
 										break;
 									}
 									_sessionType.append(1, *ptr);
 									*ptr++;
 									fo_inputBufferIndex++;
 									break;
-		case State::SESSION_CODE:
+		case State::DATE:			if(*ptr == '|'){
+										if(_date.size() < 1){
+											_date.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_date.append(1, '\0');
+										state = State::SESSION_CODE;
+										break;
+									}
+									_date.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
 									break;
-			default:				break;
+		case State::SESSION_CODE:	if(*ptr == '|'){
+										if(_sessionCode.size() < 1){
+											_sessionCode.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_sessionCode.append(1, '\0');
+										state = State::DOY;
+										break;
+									}
+									_sessionCode.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::DOY:			if(*ptr == '|'){
+										if(_doy.size() < 1){
+											_doy.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_doy.append(1, '\0');
+										state = State::TIME;
+										break;
+									}
+									_doy.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::TIME:			if(*ptr == '|'){
+										if(_time.size() < 1){
+											_time.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_time.append(1, '\0');
+										state = State::DUR;
+										break;
+									}
+									_time.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::DUR:			if(*ptr == '|'){
+										if(_dur.size() < 1){
+											_dur.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_dur.append(1, '\0');
+										state = State::STATIONS;
+										break;
+									}
+									_dur.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::STATIONS:		if(*ptr == '|'){
+										if(_stations.size() < 1){
+											_stations.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_stations.append(1, '\0');
+										state = State::SKED;
+										break;
+									}
+									_stations.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::SKED:			if(*ptr == '|'){
+										if(_sked.size() < 1){
+											_sked.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_sked.append(1, '\0');
+										state = State::CORR;
+										break;
+									}
+									_sked.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::CORR:			if(*ptr == '|'){
+										if(_corr.size() < 1){
+											_corr.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_corr.append(1, '\0');
+										state = State::STATUS;
+										break;
+									}
+									_corr.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::STATUS:			if(*ptr == '|'){
+										if(_status.size() < 1){
+											_status.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_status.append(1, '\0');
+										state = State::DBC_CODE;
+										break;
+									}
+									_status.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::DBC_CODE:		if(*ptr == '|'){
+										if(_dbcCode.size() < 1){
+											_dbcCode.append(1, *ptr);
+											*ptr++;
+											fo_inputBufferIndex++;
+											break;
+										}
+										_dbcCode.append(1, '\0');
+										state = State::SUBM;
+										break;
+									}
+									_dbcCode.append(1, *ptr);
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+		case State::SUBM:		if(*ptr == '|'){
+									if(_subm.size() < 1){
+										_subm.append(1, *ptr);
+										*ptr++;
+										fo_inputBufferIndex++;
+										break;
+									}
+									_subm.append(1, '\0');
+									state = State::DEL;
+									break;
+								}
+								_subm.append(1, *ptr);
+								*ptr++;
+								fo_inputBufferIndex++;
+								break;
+		case State::DEL:		if(*ptr == '|'){
+									if(_del.size() < 1){
+										_del.append(1, *ptr);
+										*ptr++;
+										fo_inputBufferIndex++;
+										break;
+									}
+									_del.append(1, *ptr);
+									_del.append(1, '\0');
+									state = State::ADD_LIST_ITEM;
+									*ptr++;
+									fo_inputBufferIndex++;
+									break;
+								}
+								_del.append(1, *ptr);
+								*ptr++;
+								fo_inputBufferIndex++;
+								break;
+
+		case State::ADD_LIST_ITEM:	addListItem();
+									clearAllBuffers();
+									state = State::READY;
+									break;
+			default:			break;
 		}
 	}
 
